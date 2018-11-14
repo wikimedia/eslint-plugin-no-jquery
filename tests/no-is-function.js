9ad4cec2c6f745295d14a056b2bb51146b09cd40
Ed Sanders <ejsanders@gmail.com>
2018-11-14 19:59:33 +0000

    Rename "no-function" to "no-is-function"

--- tests/no-is-function.js
+++ tests/no-is-function.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const rule = require('../rules/no-function')
+const rule = require('../rules/no-is-function')
 const RuleTester = require('eslint').RuleTester
 
 const error = 'Prefer typeof to $.isFunction'
 
 const ruleTester = new RuleTester()
-ruleTester.run('no-function', rule, {
+ruleTester.run('no-is-function', rule, {
   valid: ['isFunction()', 'myClass.isFunction()'],
   invalid: [
     {
