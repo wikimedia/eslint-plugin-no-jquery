7f3db1727a6922f19572616653f6bbe7f659f423
Ed S <ejsanders@gmail.com>
2018-11-30 17:21:42 +0000

    Introduce no-die/no-live rules (#19)

---
+++ tests/no-die.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const rule = require('../rules/no-die')
+const RuleTester = require('eslint').RuleTester
+
+const error = 'Prefer $.off/removeEventListener to die'
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-die', rule, {
+  valid: ['die()', '[].die()', 'div.die()', 'div.die'],
+  invalid: [
+    {
+      code: '$("div").die()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$div.die()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").first().die()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").append($("input").die())',
+      errors: [{message: error, type: 'CallExpression'}]
+    }
+  ]
+})
