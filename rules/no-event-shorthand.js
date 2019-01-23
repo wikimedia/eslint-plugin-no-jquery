058487ae72d800c5cb3f6d038743e6d08fe5fa37
Ed S <ejsanders@gmail.com>
2019-01-23 23:28:36 +0000

    Build no-event-shorthand rule using util (#60)

--- rules/no-event-shorthand.js
+++ rules/no-event-shorthand.js
@@ -2,58 +2,36 @@
 
 const utils = require('./utils.js')
 
-module.exports = {
-  meta: {
-    docs: {},
-    schema: []
-  },
-
-  create: function(context) {
-    const forbidden = [
-      // Browser
-      'error',
-      'resize',
-      'scroll',
-      // Form
-      'blur',
-      'change',
-      'focus',
-      'focusin',
-      'focusout',
-      'select',
-      'submit',
-      // Keyboard
-      'keydown',
-      'keypress',
-      'keyup',
-      // Mouse
-      'click',
-      'contextmenu',
-      'dblclick',
-      'hover',
-      'mousedown',
-      'mouseenter',
-      'mouseleave',
-      'mousemove',
-      'mouseout',
-      'mouseover',
-      'mouseup'
-    ]
-
-    return {
-      CallExpression: function(node) {
-        if (node.callee.type !== 'MemberExpression') return
-        if (node.callee.object.name === '$') return
-        if (forbidden.indexOf(node.callee.property.name) === -1) return
-
-        if (utils.isjQuery(node)) {
-          context.report({
-            node: node,
-            message:
-              'Prefer $.on or $.trigger to $.' + node.callee.property.name
-          })
-        }
-      }
-    }
-  }
-}
+module.exports = utils.createCollectionMethodRule(
+  [
+    // Browser
+    'error',
+    'resize',
+    'scroll',
+    // Form
+    'blur',
+    'change',
+    'focus',
+    'focusin',
+    'focusout',
+    'select',
+    'submit',
+    // Keyboard
+    'keydown',
+    'keypress',
+    'keyup',
+    // Mouse
+    'click',
+    'contextmenu',
+    'dblclick',
+    'hover',
+    'mousedown',
+    'mouseenter',
+    'mouseleave',
+    'mousemove',
+    'mouseout',
+    'mouseover',
+    'mouseup'
+  ],
+  node => `Prefer $.on or $.trigger to $.${node.callee.property.name}`
+)
