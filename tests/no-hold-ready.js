f21d3eb7f354f07d4239975c35271221ec8f31d3
Ed S <ejsanders@gmail.com>
2019-01-24 16:31:43 +0000

    Add rule for $.sub & $.holdReady and add $.sub to deprecated-1.7 (#61)
    Part of #32

---
+++ tests/no-hold-ready.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const rule = require('../rules/no-hold-ready')
+const RuleTester = require('eslint').RuleTester
+
+const error = '$.holdReady is not allowed'
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-holdReady', rule, {
+  valid: ['holdReady()', '"test".holdReady()', '"test".holdReady'],
+  invalid: [
+    {
+      code: '$.holdReady()',
+      errors: [{message: error, type: 'CallExpression'}]
+    }
+  ]
+})
