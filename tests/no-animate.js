9899efc100e9dd3af54bdb64afd9420af5450b80
Ed Sanders <ejsanders@gmail.com>
2019-02-20 11:02:41 +0000

    Add 'allowScroll' option on no-animate rule
    Fixes #65

--- tests/no-animate.js
+++ tests/no-animate.js
@@ -3,11 +3,29 @@
 const rule = require( '../rules/no-animate' );
 const RuleTester = require( 'eslint' ).RuleTester;
 
-const error = 'Prefer CSS transitions to $.animate';
+const error = 'Prefer CSS transitions or CSS scroll-behaviour to $.animate';
+const errorNoScroll = 'Prefer CSS transitions to $.animate';
 
 const ruleTester = new RuleTester();
 ruleTester.run( 'no-animate', rule, {
-	valid: [ 'animate()', '[].animate()', 'div.animate()', 'div.animate' ],
+	valid: [
+		'animate()',
+		'[].animate()',
+		'div.animate()',
+		'div.animate',
+		{
+			code: '$div.animate({scrollTop: 100})',
+			options: [ { allowScroll: true } ]
+		},
+		{
+			code: '$div.animate({scrollLeft: 200})',
+			options: [ { allowScroll: true } ]
+		},
+		{
+			code: '$div.animate({scrollTop: 100, scrollLeft: 200})',
+			options: [ { allowScroll: true } ]
+		}
+	],
 	invalid: [
 		{
 			code: '$("div").animate()',
@@ -24,6 +42,32 @@ ruleTester.run( 'no-animate', rule, {
 		{
 			code: '$("div").append($("input").animate())',
 			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$div.animate({scrollTop: 100})',
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$div.animate({scrollTop: 100})',
+			options: [ { allowScroll: false } ],
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$div.animate({scrollLeft: 200})',
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$div.animate({scrollTop: 100, scrollLeft: 200})',
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$div.animate({scrollTop: 100, width: 300})',
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$div.animate({scrollTop: 100, width: 300})',
+			options: [ { allowScroll: true } ],
+			errors: [ { message: errorNoScroll, type: 'CallExpression' } ]
 		}
 	]
 } );
