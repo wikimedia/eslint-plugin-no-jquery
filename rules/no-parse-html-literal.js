b7e8f41441b77378506490f56da34f6b0b8437fe
Ed S <ejsanders@gmail.com>
2019-09-20 20:20:31 +0100

    Improvements to no-parse-html-literal (#88)
    * When argument is a literal tree, evaluate it.
    * Lint against HTML literal parsing done by
      $.html/add/append methods.

--- rules/no-parse-html-literal.js
+++ rules/no-parse-html-literal.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const utils = require( './utils.js' );
+
 // HTML regex (modified from jQuery)
 const rquickExpr = /^(?:\s*(<[\w\W]+>)[^>]*)$/;
 // Single tag regex (from jQuery)
@@ -13,6 +15,16 @@ function allLiteral( node ) {
 	}
 }
 
+function joinLiteral( node ) {
+	if ( node.type === 'BinaryExpression' ) {
+		return joinLiteral( node.left ) + joinLiteral( node.right );
+	} else if ( node.type === 'Literal' ) {
+		return node.value;
+	} else {
+		throw new Error( 'Non-literal node passed to joinLiteral' );
+	}
+}
+
 module.exports = {
 	meta: {
 		docs: {},
@@ -22,15 +34,40 @@ module.exports = {
 	create: function ( context ) {
 		return {
 			CallExpression: function ( node ) {
+				let allowSingle;
 				if ( node.callee.type === 'Identifier' ) {
-					if (
-						node.callee.name !== '$' ||
-						!node.arguments[ 0 ] ||
-						node.arguments[ 0 ].type !== 'Literal'
-					) {
+					if ( !(
+						node.callee.name === '$' &&
+						node.arguments[ 0 ] &&
+						(
+							node.arguments[ 0 ].type === 'Literal' ||
+							node.arguments[ 0 ].type === 'BinaryExpression'
+						)
+					) ) {
 						return;
 					}
-					const value = node.arguments[ 0 ].value;
+					allowSingle = true;
+				} else if ( node.callee.type === 'MemberExpression' ) {
+					if (
+						node.callee.object.name === '$' &&
+						node.callee.property.name === 'parseHTML'
+					) {
+						allowSingle = false;
+					} else if (
+						[ 'html', 'append', 'add' ].indexOf( node.callee.property.name ) !== -1 &&
+						utils.isjQuery( node )
+					) {
+						allowSingle = true;
+					} else {
+						return;
+					}
+				} else {
+					return;
+				}
+
+				const arg = node.arguments[ 0 ];
+				if ( allowSingle ) {
+					const value = arg && allLiteral( arg ) && joinLiteral( arg );
 					if (
 						typeof value !== 'string' ||
 						!value ||
@@ -40,14 +77,6 @@ module.exports = {
 						return;
 					}
 				} else {
-					if (
-						node.callee.type !== 'MemberExpression' ||
-						node.callee.object.name !== '$' ||
-						node.callee.property.name !== 'parseHTML'
-					) {
-						return;
-					}
-					const arg = node.arguments[ 0 ];
 					if (
 						!arg ||
 						!allLiteral( arg )
