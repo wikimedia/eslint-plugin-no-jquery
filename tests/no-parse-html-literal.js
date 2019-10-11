c9e02610253074ac1f18e445bdce60c49edf2042
Ed S <ejsanders@gmail.com>
2019-10-11 17:10:20 +0100

    Enforce single tag style in `no-parse-html-literal` (#152)
    Fixes #139

--- tests/no-parse-html-literal.js
+++ tests/no-parse-html-literal.js
@@ -4,6 +4,8 @@ const rule = require( '../rules/no-parse-html-literal' );
 const RuleTesterAndDocs = require( '../rule-tester-and-docs' );
 
 const error = 'Prefer DOM building to parsing HTML literals';
+const errorMinimal = 'Single tag must use the format: <tagname>';
+const errorSelfClosing = 'Single tag must use the format: <tagname/>';
 
 const ruleTester = new RuleTesterAndDocs();
 ruleTester.run( 'no-parse-html-literal', rule, {
@@ -12,15 +14,37 @@ ruleTester.run( 'no-parse-html-literal', rule, {
 		'$()',
 		'$("")',
 		'$([])',
+		'$("#id > .class[attr]")',
 		'$(variable)',
 		'$(variable1 = variable2)',
 		'$(function() {})',
 		'$("<div>")',
-		'$("<div></div>")',
 		'$("<div>", {width:100})',
-		'$("<div/>")',
-		'$("<div />")',
 		'$("<" + "div" + ">")',
+		{
+			code: '$("<div>")',
+			options: [ { singleTagStyle: 'minimal' } ]
+		},
+		{
+			code: '$("<div/>")',
+			options: [ { singleTagStyle: 'self-closing' } ]
+		},
+		{
+			code: '$("<div />")',
+			options: [ { singleTagStyle: 'any' } ]
+		},
+		{
+			code: '$("<div></div>")',
+			options: [ { singleTagStyle: 'any' } ]
+		},
+		{
+			code: '$("<div>" + "</div>")',
+			options: [ { singleTagStyle: 'any' } ]
+		},
+		{
+			code: '$("<div />")',
+			options: [ { singleTagStyle: 'any' } ]
+		},
 		// $.html
 		'$div.html()',
 		'$div.html(variable)',
@@ -60,9 +84,53 @@ ruleTester.run( 'no-parse-html-literal', rule, {
 			} )
 		);
 	}, [] ).concat( [
-		// In addition, don't event use $.parseHTML for single tags
+		// In addition, don't even use $.parseHTML for single tags
 		{
 			code: '$.parseHTML("<div>")',
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		// singleTagStyle
+		{
+			code: '$("<div/>")',
+			errors: [ { message: errorMinimal, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div></div>")',
+			errors: [ { message: errorMinimal, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div/>")',
+			options: [ { singleTagStyle: 'minimal' } ],
+			errors: [ { message: errorMinimal, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div></div>")',
+			options: [ { singleTagStyle: 'minimal' } ],
+			errors: [ { message: errorMinimal, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div>")',
+			options: [ { singleTagStyle: 'self-closing' } ],
+			errors: [ { message: errorSelfClosing, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div />")',
+			options: [ { singleTagStyle: 'self-closing' } ],
+			errors: [ { message: errorSelfClosing, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div></div>")',
+			options: [ { singleTagStyle: 'self-closing' } ],
+			errors: [ { message: errorSelfClosing, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div attr=val>")',
+			options: [ { singleTagStyle: 'any' } ],
+			errors: [ { message: error, type: 'CallExpression' } ]
+		},
+		{
+			code: '$("<div><div>")',
+			options: [ { singleTagStyle: 'any' } ],
 			errors: [ { message: error, type: 'CallExpression' } ]
 		}
 	] )
