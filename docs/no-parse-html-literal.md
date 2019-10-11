31e809687827fbcb8d7775bdf73d1df973bd39e0
Ed Sanders <ejsanders@gmail.com>
2019-10-11 14:52:25 +0100

    Enforce single tag style in `no-parse-html-literal`
    Fixes #139

--- docs/no-parse-html-literal.md
+++ docs/no-parse-html-literal.md
@@ -2,6 +2,12 @@
 
 Disallows parsing of HTML literal strings using either the jQuery method $() or $.parseHTML. Single tags are still allowed for creating new nodes as these don't tirgger the HTML parser. DOM build and manipulation methods should be used instead.
 
+The format of single tags can be specified using the `singleTagStyle` option:
+* `"minimal"` (default) no whitespace or self-closing i.e. `<div>`
+* `"self-closing"` no whitespace and self-closing i.e. `<div/>`
+* `"any"` no style enforced
+
+
 ## Rule details
 
 ❌ The following patterns are considered errors:
@@ -27,6 +33,24 @@ $.parseHTML( '<div attr=val>' );
 $.parseHTML( '<div attr=val />' );
 $.parseHTML( '<div>' + 'content' + '</div>' );
 $.parseHTML( '<div>' );
+$( '<div/>' );
+$( '<div></div>' );
+```
+❌ With `[{"singleTagStyle":"minimal"}]` options:
+```js
+$( '<div/>' );
+$( '<div></div>' );
+```
+❌ With `[{"singleTagStyle":"self-closing"}]` options:
+```js
+$( '<div>' );
+$( '<div />' );
+$( '<div></div>' );
+```
+❌ With `[{"singleTagStyle":"any"}]` options:
+```js
+$( '<div attr=val>' );
+$( '<div><div>' );
 ```
 
 ✔️ The following patterns are not considered errors:
@@ -34,14 +58,12 @@ $.parseHTML( '<div>' );
 $();
 $( '' );
 $( [] );
+$( '#id > .class[attr]' );
 $( variable );
 $( variable1 = variable2 );
 $( function () {} );
 $( '<div>' );
-$( '<div></div>' );
 $( '<div>', { width: 100 } );
-$( '<div/>' );
-$( '<div />' );
 $( '<' + 'div' + '>' );
 $div.html();
 $div.html( variable );
@@ -51,4 +73,19 @@ $div.add( variable );
 $.parseHTML( variable );
 $.parseHTML( variable1 + variable2 );
 $.parseHTML( 'string' + variable );
+```
+✔️ With `[{"singleTagStyle":"minimal"}]` options:
+```js
+$( '<div>' );
+```
+✔️ With `[{"singleTagStyle":"self-closing"}]` options:
+```js
+$( '<div/>' );
+```
+✔️ With `[{"singleTagStyle":"any"}]` options:
+```js
+$( '<div />' );
+$( '<div></div>' );
+$( '<div>' + '</div>' );
+$( '<div />' );
 ```
--- rules/no-parse-html-literal.js
+++ rules/no-parse-html-literal.js
@@ -6,6 +6,8 @@ const utils = require( './utils.js' );
 const rquickExpr = /^(?:\s*(<[\w\W]+>)[^>]*)$/;
 // Single tag regex (from jQuery)
 const rsingleTag = /^<([a-z][^/\0>:\x20\t\r\n\f]*)[\x20\t\r\n\f]*\/?>(?:<\/\1>|)$/i;
+const rsingleTagMinimal = /^<([a-z][^/\0>:\x20\t\r\n\f]*)>$/i;
+const rsingleTagSelfClosing = /^<([a-z][^/\0>:\x20\t\r\n\f]*)\/>$/i;
 
 function allLiteral( node ) {
 	if ( node.type === 'BinaryExpression' ) {
@@ -30,15 +32,31 @@ module.exports = {
 		docs: {
 			description: 'Disallows parsing of HTML literal strings using either the jQuery method $() or $.parseHTML. ' +
 				'Single tags are still allowed for creating new nodes as these don\'t tirgger the HTML parser. ' +
-				'DOM build and manipulation methods should be used instead.'
+				'DOM build and manipulation methods should be used instead.\n\n' +
+				'The format of single tags can be specified using the `singleTagStyle` option:\n' +
+				'* `"minimal"` (default) no whitespace or self-closing i.e. `<div>`\n' +
+				'* `"self-closing"` no whitespace and self-closing i.e. `<div/>`\n' +
+				'* `"any"` no style enforced\n'
 		},
-		schema: []
+		schema: [
+			{
+				type: 'object',
+				properties: {
+					singleTagStyle: {
+						enum: [ 'minimal', 'self-closing', 'any' ]
+					}
+				},
+				additionalProperties: false
+			}
+		]
 	},
 
 	create: function ( context ) {
 		return {
 			CallExpression: function ( node ) {
-				let allowSingle;
+				let allowSingle,
+					message = 'Prefer DOM building to parsing HTML literals';
+
 				if ( node.callee.type === 'Identifier' ) {
 					if ( !(
 						utils.isjQueryConstructor( context, node.callee.name ) &&
@@ -72,26 +90,38 @@ module.exports = {
 				const arg = node.arguments[ 0 ];
 				if ( allowSingle ) {
 					const value = arg && allLiteral( arg ) && joinLiterals( arg );
-					if (
-						typeof value !== 'string' ||
-						!value ||
-						!rquickExpr.exec( value ) ||
-						rsingleTag.exec( value )
-					) {
+					if ( !( typeof value === 'string' && value ) || !rquickExpr.exec( value ) ) {
+						// Empty or non-string, or non-HTML
 						return;
 					}
-				} else {
-					if (
-						!arg ||
-						!allLiteral( arg )
-					) {
-						return;
+					if ( rsingleTag.exec( value ) ) {
+						// Single tag
+						const singleTagStyle = ( context.options[ 0 ] && context.options[ 0 ].singleTagStyle ) || 'minimal';
+						if ( singleTagStyle === 'minimal' ) {
+							if ( !rsingleTagMinimal.exec( value ) ) {
+								message = 'Single tag must use the format: <tagname>';
+							} else {
+								return;
+							}
+						} else if ( singleTagStyle === 'self-closing' ) {
+							if ( !rsingleTagSelfClosing.exec( value ) ) {
+								message = 'Single tag must use the format: <tagname/>';
+							} else {
+								return;
+							}
+						} else {
+							// singleTagStyle === 'any'
+							return;
+						}
 					}
+				} else if ( !( arg && allLiteral( arg ) ) ) {
+					// Non literals passed to $.parseHTML
+					return;
 				}
 
 				context.report( {
 					node: node,
-					message: 'Prefer DOM building to parsing HTML literals'
+					message: message
 				} );
 			}
 		};
