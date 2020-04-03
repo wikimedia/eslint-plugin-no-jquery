526f9c29ce388095ea55a009a5784484086d867d
Ed Sanders <ejsanders@gmail.com>
2020-04-03 15:46:45 +0100

    Docs: Build lists in README.md automatically
    Fixes #200

---
+++ build-readme.js
@@ -0,0 +1,26 @@
+const fs = require( 'fs' );
+const template = fs.readFileSync( 'README.md.template', { encoding: 'UTF8' } );
+const allRules = require( './index' ).rules;
+
+function getRules( deprecated ) {
+	return Object.keys( allRules ).sort().map(
+		( rule ) => {
+			const docs = allRules[ rule ].meta.docs;
+			if ( !!docs.deprecated === deprecated ) {
+				return '* [`no-jquery/' + rule + '`](docs/' + rule + '.md)' + (
+					docs.replacedBy ?
+						' (use [`no-jquery/' + docs.replacedBy + '`](docs/' + docs.replacedBy + '.md))' :
+						''
+				);
+			}
+		}
+	).filter( ( rule ) => rule ).join( '\n' );
+}
+
+fs.writeFileSync(
+	'README.md',
+	'<!-- This file is built by build-readme.js. Do not edit it directly; edit README.md.template instead. -->\n' +
+	template
+		.replace( '<!-- rules -->', getRules( false ) )
+		.replace( '<!-- deprecated -->', getRules( true ) )
+);
