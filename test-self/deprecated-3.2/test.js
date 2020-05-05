751770b1fda3f58224e1c2b29288395505f7f25f
Ed S <ejsanders@gmail.com>
2020-05-05 15:22:26 +0100

    Profile fix: Fix override of `no-event-shorthand` in `deprecated-3.5`
    * Replace rulesdir plugin with self
    
    * Profile fix: Fix override of `no-event-shorthand` in `deprecated-3.5`
    
    Add test fixures in `test-all/` and rename to `test-self/`.
    
    * Docs: Ignore "empty" config options

---
+++ test-self/deprecated-3.2/test.js
@@ -0,0 +1,42 @@
+/* 3.5 */
+$x.ajaxStart();
+
+/* 3.4 */
+$( ':first' );
+
+/* 3.3 */
+$x.click();
+
+/* 3.2 */
+// eslint-disable-next-line self/no-hold-ready
+$.holdReady();
+
+// eslint-disable-next-line self/no-is-array
+$.isArray();
+
+// eslint-disable-next-line self/no-node-name
+$.nodeName();
+
+/* 3.0 */
+// eslint-disable-next-line self/no-bind
+$x.bind();
+
+/* 1.10 */
+// eslint-disable-next-line self/no-context-prop
+$x.context;
+
+/* 1.9 */
+// eslint-disable-next-line self/no-support
+$.support;
+
+/* 1.8 */
+// eslint-disable-next-line self/no-and-self
+$x.andSelf();
+
+/* 1.7 */
+// eslint-disable-next-line self/no-live
+$x.live( fn );
+
+/* 1.3 */
+// eslint-disable-next-line self/no-browser
+$.browser;
