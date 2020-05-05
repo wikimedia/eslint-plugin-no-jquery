aef290259f357103b0041c52502c368e3f591c8b
Ed S <ejsanders@gmail.com>
2020-05-05 16:06:02 +0100

    Add real test coverage for index.js

---
+++ tests/index.js
@@ -0,0 +1,14 @@
+/* eslint-env mocha */
+
+const index = require( '../src/index' );
+const assert = require( 'assert' );
+const fs = require( 'fs' );
+
+describe( 'index', () => {
+	it( 'index.js contains all rules', ( done ) => {
+		fs.readdir( './src/rules', ( error, files ) => {
+			assert.strictEqual( files.length, Object.keys( index.rules ).length );
+			done();
+		} );
+	} );
+} );
