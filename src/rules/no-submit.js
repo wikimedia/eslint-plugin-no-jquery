1d619df7437f58dfbfe92baed749f62a37d6c7d3
Ed Sanders <ejsanders@gmail.com>
2021-10-20 14:28:33 +0100

    Deprecate `no-submit` rule for `no-event-shorthand`
    No other event shorthand has its own rule. If there is a need
    for this we should add options to the `no-event-shorthand` rule.

--- src/rules/no-submit.js
+++ src/rules/no-submit.js
@@ -4,5 +4,6 @@ const utils = require( '../utils.js' );
 
 module.exports = utils.createCollectionMethodRule(
 	'submit',
-	'Prefer `EventTarget#dispatchEvent` + `HTMLFormElement#submit` to `.submit`'
+	'Prefer `EventTarget#dispatchEvent` + `HTMLFormElement#submit` to `.submit`',
+	{ deprecated: [ 'no-event-shorthand' ] }
 );
