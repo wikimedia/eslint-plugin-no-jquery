142e4a88c0dd564c4cdeea1366399149e49b0e01
Ed Sanders <ejsanders@gmail.com>
2023-11-13 19:09:51 +0000

    Drop Node 12 support

--- tools/rule-tester.js
+++ tools/rule-tester.js
@@ -1,20 +1,4 @@
 'use strict';
 
-const DocGenRuleTester = require( 'eslint-docgen' ).RuleTester;
-const noFix = process.argv.includes( '--nofix' );
-
-class RuleTester extends DocGenRuleTester {
-	run( name, rule, tests ) {
-		if ( noFix ) {
-			tests.valid.forEach( ( test ) => {
-				delete test.output;
-			} );
-			tests.invalid.forEach( ( test ) => {
-				delete test.output;
-			} );
-		}
-		return super.run.call( this, name, rule, tests );
-	}
-}
-
-module.exports = RuleTester;
+// TODO: This no longer overrides RuleTester so can be deleted and replaced
+module.exports = require( 'eslint-docgen' ).RuleTester;
